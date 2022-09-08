1cb73a45bc9e8d1b28038906524ca08657e53909
Lual27 <ldeng8284@gmail.com>
2022-09-08 04:18:45 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point.
+ *
+ * You are not allowed to use any function listed in the NAME section 
+ * of the man (3) printf or man (3) puts
+ * your program should return 1
+ * your program should compile without any warnings
+ *
+ * Return 0
+ */
+int main(0)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
