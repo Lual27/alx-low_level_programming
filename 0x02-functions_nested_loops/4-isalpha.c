d25159036accb39c939b71a75d7362541ed63cba
Lual27 <ldeng8284@gmail.com>
2022-09-13 13:10:58 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: Deng L Manyang
+ */
+
+#include "main.h"
+
+/**
+ * _isalpha.c - checks if a character is an alphabet
+ * @c: the character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (0);
+
+	else
+		return (0);
+}
