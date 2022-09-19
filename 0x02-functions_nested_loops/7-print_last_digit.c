6e6a101c859c122e67a09ba2648e81902ed4f3d9
Lual27 <ldeng8284@gmail.com>
2022-09-19 13:41:30 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that computes the absolute value of an integer
+ * @n: is the int that will use for the arguement of the function
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	if (n < 0)
+	{
+		lastDigit = (-1 * (n % 10));
+		_putchar(lastDigit + '0');
+		return (lastDigit);
+	}
+	else
+	{
+		lastDigit = (n % 10);
+		_putchar(lastDigit + '0');
+		return (lastDigit);
+	}
+}
