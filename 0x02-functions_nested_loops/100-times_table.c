a9e99925868913bf74c5d35f9a9271a45d557405
Lual27 <ldeng8284@gmail.com>
2022-09-19 14:55:40 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,8 +37,8 @@ void print_times_table(int n)
 				}
 				else
 				{
-					_putchar(((op / 100) % 10 ) + 48);
-					_putchar(((op / 10) % 10 + 48);
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
 					_putchar((op % 10) + 48);
 				}
 			}
