9b994a135911cc86ae06dbdc0404de807d577a4e
Lual27 <ldeng8284@gmail.com>
2022-09-19 17:04:22 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,12 +3,14 @@
 /**
  * _puts - prints a string to stdout.
  * @str: The string to be printed.
+ *
+ * Return: void
  */
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-		_putchar(*str);
+	_putchar(*str);
 	}
 	_putchar('\n');
 }
