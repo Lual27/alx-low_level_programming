ba6cf252f9a96a6e755ea1af70c34b915ac17d3f
Lual27 <ldeng8284@gmail.com>
2022-09-22 05:23:59 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@
 
 void puts_half(char *str)
 {
-	int len, n , i;
+	int len, n, i;
 
 	len = 0;
 
@@ -26,7 +26,7 @@ void puts_half(char *str)
 	}
 	else if (len % 2)
 	{
-		for (n = (len -1) / 2; n < len - 1; n++)
+		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
 			putchar(str[n + 1]);
 		}
