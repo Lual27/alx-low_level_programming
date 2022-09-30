e550c18568f6801cfe973477346c72a247ca1cea
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-09-30 21:04:34 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,30 +2,30 @@
 #include <stdio.h>
 
 /**
- *  * _strstr - Finds the first occuerence of the substring in thestring haystack.
- *   *
- *    * @haystack: Parameter for haystack
- *     * @needle: Parameter for needle
- *      *
- *       * Return: Pointer to the beginning of the located substring
- *        */
+ * _strstr - Finds the first occuerence of the substring in thestring haystack.
+ *
+ * @haystack: Parameter for haystack
+ * @needle: Parameter for needle
+ *
+ * Return: Pointer to the beginning of the located substring
+ */
 char *_strstr(char *haystack, char *needle)
 {
-		while (*haystack != '\0')
-				{
-							char *beginning = haystack;
-									char *pattern = needle;
+	while (*haystack != '\0')
+	{
+		char *beginning = haystack;
+		char *pattern = needle;
 
-											while (*pattern == *haystack && *pattern != '\0' && *haystack != '\0')
-														{
-																		haystack++;
-																					pattern++;
-																							}
+		while (*pattern == *haystack && *pattern != '\0' && *haystack != '\0')
+		{
+			haystack++;
+			pattern++;
+		}
 
-													if (*pattern == '\0')
-																	return (beginning);
-															haystack = beginning + 1;
-																}
+		if (*pattern == '\0')
+			return (beginning);
+		haystack = beginning + 1;
+	}
 
-			return ('\0');
+	return ('\0');
 }
