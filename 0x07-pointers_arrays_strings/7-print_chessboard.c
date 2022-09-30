e974face99c4b98f27c78ee9efa5e5be3a15b1a7
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-09-30 21:07:38 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
- *  * print_chessboard - Function that prints the chess board.
- *   *
- *    * @a: Pointer to array of characters.
- *     *
- *      * Return: Void
- *       */
+ * print_chessboard - Function that prints the chess board.
+ *
+ * @a: Pointer to array of characters.
+ *
+ * Return: Void
+ */
 
 void print_chessboard(char (*a)[8])
 {
-		unsigned int i, j;
+	unsigned int i, j;
 
-			for (i = 0; i < 8; i++)
-					{
-								for (j = 0; j < 8; j++)
-												_putchar(a[i][j]);
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
 
-										_putchar('\n');
-											}
+		_putchar('\n');
+	}
 }
