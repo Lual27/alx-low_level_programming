553f2d4e09faca0df917958a7df6e488e7c6d36f
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-09-30 19:42:15 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,14 +3,14 @@
 #include "main.h"
 
 /**
- *  * _strcmp - Function that compares two strings.
- *   * @s1: Pointer to null-terminated first string.
- *    * @s2: Pointer to null-terminated second string.
- *     *
- *      * Return: 0 (Success).
- *       */
+ * _strcmp - Function that compares two strings.
+ * @s1: Pointer to null-terminated first string.
+ * @s2: Pointer to null-terminated second string.
+ *
+ * Return: 0 (Success).
+ */
 
 int _strcmp(char *s1, char *s2)
 {
-		return (strcmp(s1, s2));
+	return (strcmp(s1, s2));
 }
