5b74f1c5e48bf5a41e7182fb1e8c74d99f5fc3af
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-09-30 20:39:02 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
- *  * _memcpy - Function that copies memory area.
- *   * @dest: Pointer to the object to copy to.
- *    * @src: Pointer to the object to copy from.
- *     * @n: Number of bytes to copy.
- *      *
- *       * Return: A pointer to dest or 0 on success.
- *        */
+ * _memcpy - Function that copies memory area.
+ * @dest: Pointer to the object to copy to.
+ * @src: Pointer to the object to copy from.
+ * @n: Number of bytes to copy.
+ *
+ * Return: A pointer to dest or 0 on success.
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-		unsigned int i;
+	unsigned int i;
 
-			for (i = 0; i < n; i++)
-					{
-								*(dest + i) = *(src + i);
-									}
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
 
-				return (dest);
+	return (dest);
 }
