7c78ad4cf1ac452bf2b9d8a0e25cc815c5992268
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-09-30 19:51:18 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,62 +2,62 @@
 #include <stdio.h>
 
 /**
- *  * isLower - Determines whether ASCII is lowercase
- *   * @c: Character
- *    *
- *     * Return: 1 if true, 0 if false
- *      */
+ * isLower - Determines whether ASCII is lowercase
+ * @c: Character
+ *
+ * Return: 1 if true, 0 if false
+ */
 
 int isLower(char c)
 {
-		return (c >= 97 && c <= 122);
+	return (c >= 97 && c <= 122);
 }
 
 /**
- *  * isDelimiter - Determines whether ASCII is a delimeter
- *   * @c: Character
- *    *
- *     * Return: 1 if true, 0 if false
- *      */
+ * isDelimiter - Determines whether ASCII is a delimeter
+ * @c: Character
+ *
+ * Return: 1 if true, 0 if false
+ */
 
 int isDelimiter(char c)
 {
-		int i;
-			char delimeter[] = " \t\n,.!?\"(){}";
+	int i;
+	char delimeter[] = " \t\n,.!?\"(){}";
 
-				for (i = 0; i < 12; i++)
-							if (c == delimeter[i])
-											return (1);
+	for (i = 0; i < 12; i++)
+		if (c == delimeter[i])
+			return (1);
 
-					return (0);
+	return (0);
 }
 
 /**
- *  * cap_string - Capitalizes all words of a string
- *   * @s: Input string
- *    *
- *     * Return: String with capitalized words
- *      */
+ * cap_string - Capitalizes all words of a string
+ * @s: Input string
+ *
+ * Return: String with capitalized words
+ */
 
 char *cap_string(char *s)
 {
-		char *ptr = s;
-			int foundDelimit = 1;
+	char *ptr = s;
+	int foundDelimit = 1;
 
-				while (*s)
-						{
-									if (isDelimiter(*s))
-													foundDelimit = 1;
-											else if (isLower(*s) && foundDelimit)
-														{
-																		*s -= 32;
-																					foundDelimit = 0;
-																							}
-													else
-																{
-																				foundDelimit = 0;
-																						}
-															s++;
-																}
-					return (ptr);
+	while (*s)
+	{
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+		{
+			foundDelimit = 0;
+		}
+		s++;
+	}
+	return (ptr);
 }
