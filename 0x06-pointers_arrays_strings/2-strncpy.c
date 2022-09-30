1b3a6a3f8655abbae9de472c5a7893e7e6a05062
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-09-30 19:38:12 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,15 +3,15 @@
 #include "main.h"
 
 /**
- *  * _strncpy - Function that copies a string.
- *   * @dest: Pointer to character array to copy to.
- *    * @src: Pointer to character array to copy from.
- *     * @n: Maximum number of characters to copy.
- *      *
- *       * Return: A copy of dest.
- *        */
+ * _strncpy - Function that copies a string.
+ * @dest: Pointer to character array to copy to.
+ * @src: Pointer to character array to copy from.
+ * @n: Maximum number of characters to copy.
+ *
+ * Return: A copy of dest.
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-		return (strncpy(dest, src, n));
+	return (strncpy(dest, src, n));
 }
