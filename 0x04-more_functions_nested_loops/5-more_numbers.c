2e19ae17584f19341bdc46e5d393fa55e424e70e
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-09-30 18:40:08 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * more_numbers - Print numbers between 0 to 14
@@ -8,16 +9,18 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int a, b;
 
-	for (i = 0; i < 10; i++)
+	for (a = 0; a < 10; a++)
 	{
-		for (j = 0; j < 15; j++)
+		for (b = 0; b <= 14; b++)
 		{
-			if (j >= 10)
-				putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
+			if (b > 9)
+			{
+				_putchar((b / 10) + '0');
+			}
+			_putchar((b % 10) + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
