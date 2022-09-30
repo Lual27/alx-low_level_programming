451dd4238c24e13f8a0ad63b5119417d2c5208a7
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-09-30 21:02:11 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,30 +2,30 @@
 #include <stdio.h>
 
 /**
- *  * _strpbrk - Bytes
- *   *
- *    * @s: Pointer to char
- *     * @accept: Pointer to char
- *      *
- *       * Return: NULL
- *        */
+ * _strpbrk - Bytes
+ *
+ * @s: Pointer to char
+ * @accept: Pointer to char
+ *
+ * Return: NULL
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
-		int i;
+	int i;
 
-			while (*s)
-					{
-								for (i = 0; accept[i]; i++)
-											{
-															if (*s == accept[i])
-																			{
-																								return (s);
-																											}
-																	}
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
 
-										s++;
-											}
+		s++;
+	}
 
-				return (NULL);
+	return (NULL);
 }
