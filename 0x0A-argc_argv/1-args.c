7e3963c804aabbbe9dcfb795ad976b17987ac6f2
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-10-01 10:24:17 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,17 +2,17 @@
 #include "main.h"
 
 /**
- *  * main - Program that prints thenumber of args passed to it.
- *   *
- *    * @argc: Number of arguments passed to the function.
- *     * @argv: Argument vector of pointers to strings.
- *      *
- *       * Return: 0
- *        */
+ * main - Program that prints thenumber of args passed to it.
+ *
+ * @argc: Number of arguments passed to the function.
+ * @argv: Argument vector of pointers to strings.
+ *
+ * Return: 0
+ */
 
 int main(int argc, char *argv[]__attribute__((unused)))
 {
-		printf("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 
-			return (0);
+	return (0);
 }
