9056578a48fac9a7619ab14144e8224063515e72
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-10-01 10:28:34 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,23 +3,23 @@
 #include "main.h"
 
 /**
- *  * main - Program that adds two numbers.
- *   * @argc: number of arguments passed to the function.
- *    * @argv: argument vector of pointers to strings.
- *     *
- *      * Return: 0 (Success) or 1 (Error)
- *       */
+ * main - Program that adds two numbers.
+ * @argc: number of arguments passed to the function.
+ * @argv: argument vector of pointers to strings.
+ *
+ * Return: 0 (Success) or 1 (Error)
+ */
 
 int main(int argc, char *argv[])
 {
-		if (argc != 3)
-				{
-							printf("Error\n");
+	if (argc != 3)
+	{
+		printf("Error\n");
 
-									return (1);
-										}
+		return (1);
+	}
 
-			printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
-				return (0);
+	return (0);
 }
