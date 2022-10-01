9e1057ca48702590ff084fb822b26eeb7f84bef3
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-10-01 10:26:25 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,20 +2,20 @@
 #include "main.h"
 
 /**
- *  * main - Program that prints the name of the program.
- *   *
- *    * @argc: Number of arguments passed to the function.
- *     * @argv: argument vector of pointers to strings.
- *      *
- *       * Return: 0
- *        */
+ * main - Program that prints the name of the program.
+ *
+ * @argc: Number of arguments passed to the function.
+ * @argv: argument vector of pointers to strings.
+ *
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
-		while (argc--)
-				{
-							printf("%s\n", *argv++);
-								}
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
 
-			return (0);
+	return (0);
 }
