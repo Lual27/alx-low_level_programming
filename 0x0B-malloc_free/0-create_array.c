f1472a1de645221ea02e1fe2c0de2729b157c27a
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-10-03 20:16:24 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,28 +3,28 @@
 #include "main.h"
 
 /**
- *  * create_array - creates an array of chars.
- *   *
- *    * @size: argument for size of the array
- *     * @c: argument for char
- *      *
- *       * Return: 0.
- *        */
+ * create_array - creates an array of chars.
+ *
+ * @size: argument for size of the array
+ * @c: argument for char
+ *
+ * Return: 0.
+ */
 
 char *create_array(unsigned int size, char c)
 {
-		char *buffer = malloc(sizeof(c) * size);
-			unsigned int i;
+	char *buffer = malloc(sizeof(c) * size);
+	unsigned int i;
 
-				if (size == 0 || buffer == NULL)
-						{
-									return (NULL);
-										}
+	if (size == 0 || buffer == NULL)
+	{
+		return (NULL);
+	}
 
-					for (i = 0; i < size; i++)
-							{
-										buffer[i] = c;
-											}
+	for (i = 0; i < size; i++)
+	{
+		buffer[i] = c;
+	}
 
-						return (buffer);
+	return (buffer);
 }
