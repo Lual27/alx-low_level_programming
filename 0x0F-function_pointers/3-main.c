a74bfd1fd0c01700a2a5e17dcfc54519762596d0
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-10-13 15:06:51 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,39 +3,39 @@
 #include "3-calc.h"
 
 /**
- *  * main - Entry point
- *   *
- *    * @argc: Length of command line arguments
- *     * @argv: Double pointer to cli arguments
- *      *
- *       * Return: 0
- *        */
+ * main - Entry point
+ *
+ * @argc: Length of command line arguments
+ * @argv: Double pointer to cli arguments
+ *
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
-		int (*func_ptr)(int, int);
+	int (*func_ptr)(int, int);
 
-			if (argc != 4)
-					{
-								printf("Error\n");
-										exit(98);
-											}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-				if (argv[2][1] != '\0')
-						{
-									printf("Error\n");
-											exit(99);
-												}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-					func_ptr = get_op_func(argv[2]);
+	func_ptr = get_op_func(argv[2]);
 
-						if (func_ptr == NULL)
-								{
-											printf("Error\n");
-													exit(99);
-														}
+	if (func_ptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-							printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
 
-								return (0);
+	return (0);
 }
