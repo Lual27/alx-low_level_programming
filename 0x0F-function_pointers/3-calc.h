b64048baf4393132cb6f5fcef1360117e1cf2e5e
Deng L. Manyang <111116411+Lual27@users.noreply.github.com>
2022-10-13 15:04:31 +0300

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,16 +2,16 @@
 #define OP_FUNCTIONS_
 
 /**
- *  * struct op - Struct op
- *   *
- *    * @op: THe operator
- *     * @f: THe function associated
- *      */
+ * struct op - Struct op
+ *
+ * @op: THe operator
+ * @f: THe function associated
+ */
 
 typedef struct op
 {
-		char *op;
-			int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
